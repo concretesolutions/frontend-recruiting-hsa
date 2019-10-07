1c6b274d41f8e23414cc2652eda749e89e9d1780
Richard <richard.eneene@gmail.com>
2019-10-06 21:26:40 -0300

    Add Routes and components
    - Set routes for /home, /details & not-found.
    - Add the list of components and modules used.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { GitUserProfileComponent } from './components/git-user-profile/git-user-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomepageComponent },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: 'detail', component: GitUserProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
