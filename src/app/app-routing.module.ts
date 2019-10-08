cf7c7cc712d6fb84e454ffb1beae229e0dd699af
German <gmaluenda.alfaro@gmail.com>
2019-10-08 10:15:01 -0300

    feat: se agregan componentes para las vistas del buscador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { SearchComponent } from './features/search/search.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { DetailsComponent } from './features/details/details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
+  {
+    path: 'notfound',
+    component: NotFoundComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
+  },
+  { path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './features/search/search.component';
+import { HomeComponent } from './features/home/home.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { ResultsComponent } from './features/results/results.component';
+import { DetailsComponent } from './features/details/details.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ResultsComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
