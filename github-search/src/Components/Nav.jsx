c02972d247f16509efc9413e201ad40bb7438a84
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-10-16 12:38:00 -0300

    Se trae la data desde github

--- github-search/src/Components/Nav.jsx
+++ github-search/src/Components/Nav.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 export const Nav = () => {
+    const [search, setSearch] = useState('')
+    const [users, setUsers] = useState([])
+    const [repos, setRepos] = useState([])
+
+
+    const searchUser = async () => {
+        console.log(search)
+
+        try {
+            const dataUsers = await axios(`https://api.github.com/users/${search}`)
+            const dataRepos = await axios(`https://api.github.com/users/${search}/repos`)
+            setUsers(dataUsers)
+            setRepos(dataRepos)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    console.log(users)
+    console.log(repos)
+    
     return (
-        <div className="nav-container">
-            <div className="nav-containerTittle">
-                <h3 className="nav-partOneTitle">Github</h3>
-                <h3 className="nav-partTwoTitle">Search</h3>
-            </div>
-            <div className="nav-containerInput">
-                <div className="nav-borderInput">
-                    <input className="nav-input" type="text"/>
-                    <button className="nav-btnInput">
-                        <img className="nav-imgInput" src="https://i.ibb.co/QD0q88v/lupa.png" alt="Lupa de búsqueda"/>
-                    </button>
+        <>
+            <div className="nav-container">
+                <div className="nav-containerTittle">
+                    <h3 className="nav-partOneTitle">Github</h3>
+                    <h3 className="nav-partTwoTitle">Search</h3>
+                </div>
+                <div className="nav-containerInput">
+                    <div className="nav-borderInput">
+                        <input className="nav-input" placeholder="Nombre de usuario" value={search} type="text" onChange={ (e) => setSearch(e.target.value)} />
+                        <button className="nav-btnInput" onClick={() => searchUser() }>
+                            <img className="nav-imgInput" src="https://i.ibb.co/QD0q88v/lupa.png" alt="Lupa de búsqueda"/>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
+
+export default Nav;
