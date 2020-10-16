32e3a176e8b2157680da7aef268f3be51dec7375
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-10-15 21:17:56 -0300

    rutas

--- github-search/src/App.jsx
+++ github-search/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import { Home } from './Views/Home';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        {/* <Route path="/detalles">
+        </Route>
+
+        <Route path="/noEncontrado">
+        </Route> */}
+
+      </Switch>
+    </Router>
   );
 }
 
