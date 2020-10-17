10356a07432b48226a18997042c30e9581e315a1
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-10-17 10:17:07 -0300

    deploy con firebase

--- github-search/src/Context/UserContext.js
+++ github-search/src/Context/UserContext.js
@@ -6,23 +6,23 @@ let { Provider, Consumer } = UserContext
 
 function UserProvider({ children }) {
 
-    const [search, setSearch] = useState('')
-    const [users, setUsers] = useState([])
-    const [repos, setRepos] = useState([])
+  const [search, setSearch] = useState('')
+  const [users, setUsers] = useState([])
+  const [repos, setRepos] = useState([])
 
-    const searchUser = async () => {
-        try {
-            const dataUsers = await axios(`https://api.github.com/users/${search}`)
-            const dataRepos = await axios(`https://api.github.com/users/${search}/repos`)
-            setUsers(dataUsers.data)
-            setRepos(dataRepos.data)
-        } catch (error) {
-            console.log(error)
-        }
+  const searchUser = async () => {
+    try {
+        const dataUsers = await axios(`https://api.github.com/users/${search}`)
+        const dataRepos = await axios(`https://api.github.com/users/${search}/repos`)
+        setUsers(dataUsers.data)
+        setRepos(dataRepos.data)
+    } catch (error) {
+        console.log(error)
     }
+  }
 
-//  Orden de repos por estrellas
-    repos.sort((a,b) => (a.stargazers_count  > b.stargazers_count  ? -1 : a.stargazers_count  < b.stargazers_count  ? 1 : 0)) 
+// Orden de repos por estrellas
+  repos.sort((a,b) => (a.stargazers_count  > b.stargazers_count  ? -1 : a.stargazers_count  < b.stargazers_count  ? 1 : 0)) 
 
   return (
     <Provider value={{ setSearch, users, repos, searchUser, search
