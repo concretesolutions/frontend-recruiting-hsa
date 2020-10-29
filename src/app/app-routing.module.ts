642fccfd1947e462ca198e62532b9d31de8f8f23
Estefany Pacheco <>
2020-10-29 20:57:04 -0300

    UPDATE | Setting app module and adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchPageComponent } from './components/pages/search-page/search-page.component';
+import { UserPageComponent } from './components/pages/user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchPageComponent },
+  {
+    path: 'search/user/:user',
+    component: UserPageComponent
+  },
+  { path: '**', redirectTo: '/search' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './components/pages/search-page/search-page.component';
+import { UserPageComponent } from './components/pages/user-page/user-page.component';
+import { HeaderComponent } from './components/commons/header/header.component';
+import { ListItemComponent } from './components/commons/list-item/list-item.component';
+import { LogoComponent } from './components/commons/logo/logo.component';
+import { SearchBarComponent } from './components/commons/search-bar/search-bar.component';
+import { UserService } from './services/user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [
+    HeaderComponent,
+    ListItemComponent,
+    LogoComponent,
+    SearchBarComponent,
+    SearchPageComponent,
+    UserPageComponent,
+    AppComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
