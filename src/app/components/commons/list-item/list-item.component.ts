642f7e08be3dacf3fec56342eca4228c74b3b403
Estefany Pacheco <>
2020-10-29 20:56:20 -0300

    UPDATE | Creating common components

---
+++ src/app/components/commons/list-item/list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  templateUrl: './list-item.component.html',
+  styleUrls: ['./list-item.component.scss']
+})
+export class ListItemComponent {
+  @Input() icon: string;
+  @Input() altImg: string;
+  @Input() text: string;
+}
---
+++ src/app/components/commons/logo/logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.scss']
+})
+export class LogoComponent implements OnInit {
+  @Input() small = false;
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('hi');
+  }
+}
