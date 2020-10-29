176f2a5b4ce55e039e69b06c637791c9704d965f
Estefany Pacheco <>
2020-10-29 20:56:35 -0300

    UPDATE | Creating pages

---
+++ src/app/components/pages/search-page/search-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  templateUrl: './search-page.component.html'
+})
+export class SearchPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/components/pages/user-page/user-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { ReposInterface, UserDataInterface } from '../../../domain/user';
+import { Route } from '@angular/compiler/src/core';
+
+@Component({
+  selector: 'app-user-page',
+  templateUrl: './user-page.component.html'
+})
+export class UserPageComponent implements OnInit {
+  public user: string;
+  public userData: UserDataInterface | undefined;
+  public userRepos: [ReposInterface];
+  public stars = 0;
+  public loading = true;
+  constructor(private route: ActivatedRoute, private service: UserService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.user = params.user;
+      this.getUser();
+    });
+  }
+  public getUser = () => {
+    this.service.getDataUser(this.user).subscribe(
+      (user: UserDataInterface) => {
+        this.loading = false;
+        this.userData = user;
+        this.service
+          .getReposUser(this.user)
+          .subscribe((repos: [ReposInterface]) => {
+            this.userRepos = repos.sort((a, b) =>
+              a.stargazers_count < b.stargazers_count ? 1 : -1
+            );
+            this.userRepos.map((repo: ReposInterface) => {
+              this.stars += repo.stargazers_count;
+            });
+          });
+      },
+      () => {
+        this.userData = undefined;
+        this.loading = false;
+      }
+    );
+  }
+}
