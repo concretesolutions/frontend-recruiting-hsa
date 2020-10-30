7128c488d0e48d2e4c415bac51d6c9c3dcdb5d45
Estefany Pacheco <>
2020-10-29 23:18:11 -0300

    UPDATE | Deleted unused functions from component

--- src/app/components/commons/logo/logo.component.ts
+++ src/app/components/commons/logo/logo.component.ts
@@ -1,15 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss']
 })
-export class LogoComponent implements OnInit {
+export class LogoComponent{
   @Input() small = false;
-  constructor() {}
-
-  ngOnInit(): void {
-    console.log('hi');
-  }
 }
