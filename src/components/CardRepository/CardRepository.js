d3feba123819b80f115a141b2029ca2ed0a33ce4
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2020-12-03 12:21:22 -0300

    ADD: CardRepository component

---
+++ src/components/CardRepository/CardRepository.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+import styles from "./CardRepository.module.scss";
+
+const CardRepository = ({ title, description, score }) => {
+  return (
+    <div className={styles.card__container}>
+      <div className={styles.card__content}>
+        <h3>{title}</h3>
+        {/* <p>{username}</p> */}
+
+        <p>{description}</p>
+        <div className={styles.card__content__score}>
+          <p>
+            <FontAwesomeIcon
+              icon={faStar}
+              size="lg"
+              className={styles.card__icon}
+            />
+            {score}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardRepository;
--- src/views/Details/Details.js
+++ src/views/Details/Details.js
@@ -4,6 +4,7 @@ import { GlobalContext } from "../../context/GlobalState";
 import Card from "../../components/Card";
 import Searchbox from "../../components/SearchBox";
 import Logo from "../../components/Logo";
+import CardRepository from "../../components/CardRepository";
 
 const Details = () => {
   const { user } = useContext(GlobalContext);
@@ -19,17 +20,39 @@ const Details = () => {
           <Searchbox />
         </div>
       </div>
-      {user.length !== 0 && (
-        <Card
-          name={user.name || user.login}
-          username={user.login}
-          followers={user.followers}
-          email={user.email}
-          bio={user.bio}
-          avatar={user.avatar_url}
-          urlProfile={user.html_url}
-        />
-      )}
+      <div className={styles.details__user__container}>
+        <div className={styles.details__user_card}>
+          {user.length !== 0 && (
+            <Card
+              name={user.name || user.login}
+              username={user.login}
+              followers={user.followers}
+              email={user.email}
+              bio={user.bio}
+              avatar={user.avatar_url}
+              urlProfile={user.html_url}
+            />
+          )}
+        </div>
+
+        <div className={styles.details__user_repository}>
+          <CardRepository
+            title="Search Movies with React"
+            description="muchas librerias React, hooks, otras"
+            score="345"
+          />
+          <CardRepository
+            title="Search Movies with React"
+            description="muchas librerias React, hooks, otras"
+            score="345"
+          />
+          <CardRepository
+            title="Search Movies with React"
+            description="muchas librerias React, hooks, otras"
+            score="345"
+          />
+        </div>
+      </div>
     </div>
   );
 };
