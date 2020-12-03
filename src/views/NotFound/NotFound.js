a115aa6ef3a30ae5ad9ab120b4194cb01de16e0e
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2020-12-03 19:34:17 -0300

    UPDATE: NotFound view

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,6 +1,23 @@
 import React from "react";
+import Logo from "../../components/Logo";
+import Searchbox from "../../components/SearchBox";
+import styles from "./NotFound.module.scss";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles.notfound__container}>
+      <div className={styles.notfound__searchbox__container}>
+        <div className={styles.notfound__logo}>
+          <Logo size="small" />
+        </div>
+        <div className={styles.notfound__searchbox}>
+          <Searchbox />
+        </div>
+      </div>
+      <div className={styles.notfound__message}>
+        <span> User not found :(</span>
+      </div>
+    </div>
+  );
 };
 export default NotFound;
