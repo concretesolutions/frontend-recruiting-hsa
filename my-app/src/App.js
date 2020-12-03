71573e8d46ace6c5d8d5ddb2ba4c640540119cc6
BarbaraMV <bmadariagav@gmail.com>
2020-12-03 08:56:53 -0300

    Init | Creación de proyecto

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Title">
+      <h1>
+        GitHub Search
+      </h1>
+    </div>
+  );
+}
+
+export default App;
