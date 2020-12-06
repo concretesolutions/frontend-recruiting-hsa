b4d7bf13654b660cc1128bbf3713d79f0a6e0138
luiscruzga <luis.cruzga@gmail.com>
2020-12-05 21:12:25 -0300

    Message display is added when there are no repositories

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -15,7 +15,7 @@ export class DetailComponent implements OnInit {
   reposUser: Repositorie[];
   totalStarts: number = 0;
   username: string = '';
-  existsRepos: string = '';
+  notExistsRepos: string = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -27,20 +27,24 @@ export class DetailComponent implements OnInit {
       (params: Params) => {
         this.username = params.username;
         if (this.username !== '') {
+          // Reset counter
+          this.totalStarts = 0;
+          // Get user info
           this.userDetailService.userInfo.subscribe(res => {
             this.userInfo = res;
           });
-    
+          // Get user repos
           this.userDetailService.reposUser.subscribe(res => {
             this.reposUser = res;
             if (this.reposUser.length > 0) {
+              // We add the number of stars in your repos
               this.totalStarts = this.reposUser.reduce((a,b) => a + b.stargazers_count, 0);
-              this.existsRepos = '';
+              this.notExistsRepos = '';
             } else {
-              this.existsRepos = 'No repositories found';
+              this.notExistsRepos = 'No repositories found';
             }
           });
-    
+          // We trigger the search for information
           this.userDetailService.getUserInfo(this.username);
         }
       }
