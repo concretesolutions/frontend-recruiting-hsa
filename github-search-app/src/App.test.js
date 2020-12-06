377a0563d73dedfeea3fe4a62a58cf58b7d77428
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-12-06 17:28:24 -0300

    Agregando icono, eliminando archivos no usados e intento de test

--- github-search-app/src/App.test.js
+++ github-search-app/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, act } from '@testing-library/react';
+import Search from './components/Search'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  global.fetch = jest.fn(() => 
+    Promise.resolve({
+      json: () => 
+        Promise.resolve({
+        avatar_url: "http://image.png", 
+        value: "userName",
+    }),
+  })
+);
+
+describe("Search", () => {
+  it("loads the user name", async () => {
+    await act(async () => render(<Search/>))
+    expect(screen.getByText("userName")).toBeInTheDocument()
+  })
+})
+
+describe("Renders elements", () => {
+  const { getByText } = render(<Search/>);
+  const tittleElement = getByText("Github");
+  expect(tittleElement).toBeInTheDocument();
+})
