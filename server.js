260d39eb352d54d3583402bc3e857982bf7f7587
narelamuller <narelamuller13@outlook.com.ar>
2020-12-08 01:28:58 -0300

    Heroku commit final

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/app-angular'));
+// Serve static files
+app.use(express.static(dirname + '/dist/app-angular'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-angular/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(dirname + '/dist/app-angular/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
+
