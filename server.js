bacb02534cb55cbb32e545b1703e56343ae2cf56
narelamuller <narelamuller13@outlook.com.ar>
2020-12-08 00:32:48 -0300

    Heroku- app

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/examen-laboral-nm'));
+app.use(express.static('./dist/app-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/examen-laboral-nm/'}),
+    res.sendFile('index.html', {root: 'dist/app-angular/'}),
 );
 
 // Start the app by listening on the default Heroku port
