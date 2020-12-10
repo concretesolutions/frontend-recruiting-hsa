e2e2bee6ed63af1615be137e9e16e96167e3c0c3
svalencia <sebastian.valencia@netbyteoss.cl>
2020-12-09 23:24:37 -0300

    Models, service and homeComponent developed (with global scss config)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './components/details/details.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  
+    path:'', 
+    component: HomeComponent,
+    pathMatch: 'full'
+ },
+ {
+    path:'user',
+    component:DetailsComponent
+ },
+ {
+    path:'user/:userName', 
+    component:DetailsComponent
+ },
+ {
+  path: '**',
+  redirectTo: ''
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
